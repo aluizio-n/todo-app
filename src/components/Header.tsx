433079cdba2ea1f2edaf95d9c5fe61ad39614af7
Aluizio N <aluizio.sln@gmail.com>
2023-01-04 16:15:59 -0400

    feat: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <StyledHeader>
+        <div>
+            <img src="./src/assets/logo.svg" alt="" />
+        </div>
+    </StyledHeader>
+  )
+}
+
+export default Header
+
+const StyledHeader = styled.div `
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 200px;
+background-color: #0D0D0D;
+`
